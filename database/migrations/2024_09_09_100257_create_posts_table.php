f2d16ea21f31b0a08c273cf3c6fa60cbae70dc2b
pezhmana <pejman.ardalani2@gmail.com>
2024-09-09 13:46:06 +0330

    add user id

--- database/migrations/2024_09_09_100257_create_posts_table.php
+++ database/migrations/2024_09_09_100257_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->longText('caption')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
