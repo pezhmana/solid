dc4130c123fc279729b20d2887b5132a3cb4e623
pezhmana <pejman.ardalani2@gmail.com>
2024-09-17 13:17:52 +0330

    add search and find func

--- app/Services/UserClass.php
+++ app/Services/UserClass.php
@@ -2,9 +2,55 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserClass
 {
-    public function getUsers(){
+    public function createUser(array $data){
+        $data['password'] = Hash::make($data['password']);
+        return User::create($data);
+    }
 
+    public function indexUser($username){
+        $user =User::where('username',$username);
+        if(!$user->exists()){
+            $user = 'user not found';
+        }else{
+            $user =$user->first();
+        }
+        return $user;
+    }
+
+    public function login(array $data)
+    {
+        $user = User::where('username',$data['username']);
+        if($user->exists()){
+            if(!Hash::check($data['password'],$user->first()->password)){
+                $token = 'password is not correct';
+            }else{
+                $token = $user->first()->createToken('token')->plainTextToken;
+            }
+        }else{
+            $token = 'user not found';
+        }
+        return $token;
+    }
+
+    public function find($username){
+        $user = User::where('username',$username)->first();
+        return $user;
+    }
+
+    public function search($id)
+    {
+        $user = User::find($id);
+        return $user;
+    }
+
+    public function shortProfile($id)
+    {
+        $user = $this->search($id)->select(['id','username']);
+        return $user;
     }
 }
