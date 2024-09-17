be2f84dcafe910568d104af6ac85ca3d2ef16334
pezhmana <pejman.ardalani2@gmail.com>
2024-09-17 13:52:45 +0330

    add fillable and relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory , InteractsWithMedia;
+
+    protected $fillable = [
+        'caption'
+    ];
 
     public function user()
     {
