ef6acdce93a7ea9dfca899e334b4373bcb2692a2
pezhmana <pejman.ardalani2@gmail.com>
2024-09-17 13:15:54 +0330

    add follow table

---
+++ database/migrations/2024_09_10_081959_create_follows_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('follower');
+            $table->unsignedBigInteger('following');
+            $table->enum('type',['request','accept']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('follows');
+    }
+};
