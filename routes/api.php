5ea3ba8f2246551d089453e8d4cdd6268fd54923
pezhmana <pejman.ardalani2@gmail.com>
2024-09-18 12:37:20 +0330

    add new route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Middleware\EnsureFollowing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +20,17 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-
+route::post('login',[UserController::class,'login'])->name('login');
 Route::group(['prefix'=>'user','middleware'=>'auth:sanctum'],function(){
-   Route::post('',[UserController::class,'createUser'])->withoutMiddleware('auth:sanctum')->name('create');
+   Route::post('/',[UserController::class,'createUser'])->withoutMiddleware('auth:sanctum')->name('create');
+   Route::get('/{username}',[UserController::class,'index'])->middleware(EnsureFollowing::class)->name('index');
+});
+
+Route::group(['prefix'=>'follow','middleware'=>'auth:sanctum'],function(){
+route::post('{following}',[FollowController::class,'send'])->name('send');
+route::get('/',[FollowController::class,'indexRequest'])->name('indexRequest');
+});
+
+Route::group(['prefix'=>'post','middleware'=>'auth:sanctum'],function(){
+    Route::post('/',[PostController::class,'create'])->name('create');
 });
